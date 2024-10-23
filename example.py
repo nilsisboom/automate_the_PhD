a811e810e8b028f5633e61d44d5de4c0bf3fd777
Nils Pauliks <n.pauliks@cml.leidenuniv.nl>
2024-10-23 16:01:33 +0200

    some commit message

---
+++ example.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def add(a, b):
+    return a + b
+
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add('space', 'ship') == 'spaceship'
+
+
+def subtract(a, b):
+    return a + b  # <--- fix this in step 8
+
+
+# uncomment the following test in step 5
+#def test_subtract():
+#    assert subtract(2, 3) == -1
